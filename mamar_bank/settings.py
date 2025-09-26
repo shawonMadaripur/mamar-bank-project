c491ae2bf0df3f8e3c80676b61c1098c721a558c
Mehedi Hasan Shuvo <102133774+shawonMadaripur@users.noreply.github.com>
2025-09-26 07:30:27 +0600

    csrf tureted origin add

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-ttez.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
